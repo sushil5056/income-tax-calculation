74d82acde9991c57cce1a7bffb68dcb0dfed94d2
sushil kumar <sushil1thakur@gmail.com>
2023-02-02 22:38:16 +0530

    secound commit

--- income-tax-calculation/src/main/java/com/incomtaxcalculation/incometaxcalculation/service/TaxCalculationTaxService.java
+++ income-tax-calculation/src/main/java/com/incomtaxcalculation/incometaxcalculation/service/TaxCalculationTaxService.java
@@ -11,9 +11,8 @@ public class TaxCalculationTaxService {
         Percentage percentage;
     public Integer getotaltaxPayble(int ctc) {
 
-        int TaxPaybaleAmount=0;
         int totalTaxPaybaleAmount=0;
-
+       int TaxPaybaleAmount=0;
         if(ctc<=700000){
             totalTaxPaybaleAmount=0;
 
@@ -21,7 +20,20 @@ public class TaxCalculationTaxService {
 
             Integer pctage=percentage.getPercentage(ctc);
 
-            15000+  percentage.getPercentage(ctc);
+            if(600000<ctc && ctc<=900000){
+
+                TaxPaybaleAmount= 15000+(ctc-600000)*pctage/100;
+            } else if (900000<ctc && ctc<=1200000) {
+                TaxPaybaleAmount= 15000+30000+(ctc-900000)*pctage/100;
+            }
+            else if (1200000<ctc && ctc<=1500000) {
+                TaxPaybaleAmount= 15000+30000+45000+(ctc-1200000)*pctage/100;
+            }
+            else{
+                TaxPaybaleAmount= 15000+30000+45000+60000+(ctc-1500000)*pctage/100;
+            }
+
+            totalTaxPaybaleAmount=TaxPaybaleAmount+TaxPaybaleAmount*4/100;
         }
         return totalTaxPaybaleAmount;
     }
